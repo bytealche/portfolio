0952a82fb20fa77d1f17c3f59bac46ee2864bbb1
bytealche <bytealche@gmail.com>
2025-11-11 16:59:05 +0530

    fix:the input for mobile device

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
-// src/App.jsx
-
 import React, { useState, useEffect } from 'react';
 import Terminal from './components/Terminal.jsx';
 import MobileFallback from './components/MobileFallback.jsx';
 
-// This is a new, more aggressive check.
-// It looks at the "User Agent" string of the browser.
-function detectIsMobile() {
-  const toCheck = [
-    /Android/i,
-    /webOS/i,
-    /iPhone/i,
-    /iPad/i,
-    /iPod/i,
-    /BlackBerry/i,
-    /Windows Phone/i,
-  ];
+/**
+ * A comprehensive, industry-standard regex to detect 
+ * mobile phone user agents.
+ */
+function detectIsMobileAgent() {
+  const regex = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|lex)|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i;
   
-  return toCheck.some((item) => navigator.userAgent.match(item));
+  // We also check a shorter regex for other common mobile patterns
+  const regex2 = /android|webos|iphone|ipod|blackberry|iemobile|opera mini/i;
+
+  return regex.test(navigator.userAgent) || regex2.test(navigator.userAgent);
 }
 
+/**
+ * A check for small screen size.
+ * We use 767px as the cutoff (standard phone/tablet portrait width)
+ */
+const mediaQuery = window.matchMedia(`(max-width: 767px)`);
+
 function App() {
-  // We only run the check once when the app loads
-  const [isMobile, setIsMobile] = useState(detectIsMobile());
+  // We combine both checks.
+  // isMobile is true IF:
+  // 1. The user agent is a known mobile device.
+  // OR
+  // 2. The screen size is very small (like a phone).
+  const [isMobile, setIsMobile] = useState(
+    detectIsMobileAgent() || mediaQuery.matches
+  );
 
-  // We don't need a resize listener anymore, as the user agent won't change.
+  // We still need a listener in case the user resizes their desktop window
+  // (or rotates a tablet)
+  useEffect(() => {
+    const handleResize = () => {
+      // Re-run the check on resize
+      setIsMobile(detectIsMobileAgent() || mediaQuery.matches);
+    };
+    
+    mediaQuery.addEventListener('change', handleResize);
+    
+    return () => {
+      mediaQuery.removeEventListener('change', handleResize);
+    };
+  }, []);
 
-  // Render the correct component based on the state
   if (isMobile) {
     return <MobileFallback />;
   }
